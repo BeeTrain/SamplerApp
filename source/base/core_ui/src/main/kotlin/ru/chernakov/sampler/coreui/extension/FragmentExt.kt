35a0991f52de6b9b1864f694c7c14fc9ba9e1ac6
Max Chernakov <41rm4kc@gmail.com>
2021-02-03 15:39:07 +0300

    00010 - Update findView delegate with lifecycle observing

--- source/base/core_ui/src/main/kotlin/ru/chernakov/sampler/coreui/extension/FragmentExt.kt
+++ source/base/core_ui/src/main/kotlin/ru/chernakov/sampler/coreui/extension/FragmentExt.kt
@@ -3,6 +3,9 @@ package ru.chernakov.sampler.coreui.extension
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
@@ -48,10 +51,39 @@ fun <T : Any> argumentNullable(): ReadWriteProperty<Fragment, T?> {
 }
 
 class FragmentFindViewDelegate<T : View>(private val id: Int) : ReadOnlyProperty<Fragment, T> {
+    private var value: T? = null
+    private val lifecycleObserver = PropertyLifecycleObserver()
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
         val view = thisRef.view ?: throw IllegalStateException("View could not be null")
-        return view.findViewById(id)
+
+        var currentValue = value
+        return if (currentValue == null) {
+            lifecycleObserver.attach(thisRef.lifecycle)
+            currentValue = view.findViewById(id)
+            value = currentValue
+
+            currentValue
+        } else {
+            currentValue
+        }
+    }
+
+    private inner class PropertyLifecycleObserver : LifecycleObserver {
+
+        private var _lifecycle: Lifecycle? = null
+
+        fun attach(lifecycle: Lifecycle) {
+            _lifecycle = lifecycle
+            _lifecycle?.addObserver(this)
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        fun onPause() {
+            value = null
+            _lifecycle?.removeObserver(this)
+            _lifecycle = null
+        }
     }
 }
 

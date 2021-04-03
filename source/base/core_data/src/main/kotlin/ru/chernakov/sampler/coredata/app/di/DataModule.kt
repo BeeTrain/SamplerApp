61b935a74555a1d5ac577af0b76dc056a30f4f2e
Max Chernakov <41rm4kc@gmail.com>
2021-04-03 17:16:03 +0300

    00035 - use SharedPreferencesProvider for DI

--- source/base/core_data/src/main/kotlin/ru/chernakov/sampler/coredata/app/di/DataModule.kt
+++ source/base/core_data/src/main/kotlin/ru/chernakov/sampler/coredata/app/di/DataModule.kt
@@ -2,9 +2,8 @@ package ru.chernakov.sampler.coredata.app.di
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
-import ru.chernakov.sampler.coredata.prefs.AppSharedPreferences
-import ru.chernakov.sampler.coredata.prefs.SharedPreferencesApi
+import ru.chernakov.sampler.coredata.prefs.SharedPreferencesProvider
 
 val dataModule = module {
-    single<SharedPreferencesApi> { AppSharedPreferences(androidContext()) }
+    single { SharedPreferencesProvider.provide(androidContext()) }
 }
--- source/base/core_data/src/main/kotlin/ru/chernakov/sampler/coredata/prefs/SharedPreferencesProvider.kt
+++ source/base/core_data/src/main/kotlin/ru/chernakov/sampler/coredata/prefs/SharedPreferencesProvider.kt
@@ -2,7 +2,7 @@ package ru.chernakov.sampler.coredata.prefs
 
 import android.content.Context
 
-class SharedPreferencesProvider {
+object SharedPreferencesProvider {
 
     fun provide(applicationContext: Context): SharedPreferencesApi {
         return AppSharedPreferences(applicationContext)
